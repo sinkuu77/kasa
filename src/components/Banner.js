028a6bee8489fc8a2c0be25aaa9fa69e178b1a22
Minyoung GWON <sinkuu@naver.com>
2023-11-26 17:40:00 +0100

    Updates

--- src/components/Banner.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Banner.module.scss'
-
-export default function Banner({ children }) {
-  return (
-    <section className={styles.section}>
-      <div className={styles.section__container}>{children}</div>
-    </section>
-  )
-}
--- src/components/Tags.js
+++
@@ -1,5 +0,0 @@
-import styles from '../styles/Tags.module.scss'
-
-export default function Tags({ tags }) {
-  return <li className={styles.tags__list}>{tags}</li>
-}
